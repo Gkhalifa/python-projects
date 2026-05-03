6bf41e795d0892d0d9f3e21c2221a6c8e1b6e55a
Gabriel Khalifa <Khalifa.ga67@gmail.com>
2026-05-02 21:18:35 -0400

    Add level 6 grading scale challenge

---
+++ grading_scale.py
@@ -0,0 +1,10 @@
+score = int(input("give a score between 0 to 100"))
+if score >= 90:
+    print("Grade: A")
+elif score >= 80 <= 89: 
+    print("Grade: B")
+elif score >= 70 <= 79: 
+    print("Grade: C")
+else:  
+    print("Grade: F")
+	
